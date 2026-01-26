dea2db4caa6ef9cc009c370ab2b697e13c03c991
GermanLahoz <lahozgerman6@gmail.com>
2026-01-26 10:09:56 +0000

    Creación del MVC de personas y actualización de validaciones

--- config/routes.php
+++ config/routes.php
@@ -15,7 +15,7 @@ $router->protectedSession('GET', '/auth/me', 'Controllers\\AuthController@me', [
 $router->protectedSession('GET', '/personas', 'Controllers\\PersonaController@index', [1,2,3,4,5]);
 $router->protectedSession('GET', '/personas/{n_funcionario}', 'Controllers\\PersonaController@show', [1,2,3,4,5]);
 $router->protectedSession('POST', '/personas', 'Controllers\\PersonaController@store', [4,5]);
-$router->protectedSession('PUT', '/personas/{n_funcionario}', 'Controllers\\PersonaController@update', [4,5]);
+$router->protectedSession('PATCH', '/personas/{n_funcionario}', 'Controllers\\PersonaController@update', [4,5]);
 $router->protectedSession('DELETE', '/personas/{n_funcionario}', 'Controllers\\PersonaController@delete', [5]);
 
 // ROL PERSONA
