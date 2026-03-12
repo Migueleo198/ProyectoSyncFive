cfdfd82fac82250c8c7d5a0d94e4eedd5df07dd5
Javier <jmirarg@gmail.com>
2026-03-12 13:28:45 +0000

    ACABE2

--- api_rest/config/config.php
+++ api_rest/config/config.php
@@ -46,7 +46,7 @@ define('BASE_PATH', '/api');
 // DATABASE
 // ----------------------------------------------------
 define('DB_HOST', '192.168.13.108:3306');
-define('DB_NAME', 'SyncFive3');
+define('DB_NAME', 'SyncFive');
 define('DB_USER', 'root');
 define('DB_PASS', '1234');
 define('DB_CHARSET', 'utf8mb4');
